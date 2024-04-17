f12f86d6615df9f1b127c375c83e936e4d86f147
iktus <akj2995@daum.net>
2024-04-17 23:00:06 +0900

    add

--- fireprj_src/backend/api_ai_.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-print ("module [backend.api_monitor] loaded")
-
-from backend.api_common_ import *
-from backend_model.table_typhoon import *
-from sqlalchemy import sql
-import random
-db = DBManager.db
--- fireprj_src/frontend_vue/src/views/index.js
+++ fireprj_src/frontend_vue/src/views/index.js
@@ -5,25 +5,6 @@ export { default as Login_FindPassword } from './Login_FindPassword'
 
 export { default as SubView } from './SubView'
 
-// export { default as Dashboard } from './Dashboard'
-// export { default as Typhoon_Manage } from './Typhoon_Manage'
-
-// export { default as Damage_Manage } from './Damage_Manage'
-
-// export { default as Statistics_Dashboard } from './Statistics_Dashboard'
-// export { default as Statistics_Report } from './Statistics_Report'
-
-// export { default as User_Manage } from './User_Manage'
-// export { default as Community_List } from './Community_List'
-// export { default as Device } from './Device'
-// export { default as Device_Detail } from './Device_Detail'
-// export { default as Settings } from './Settings'
-// export { default as Report } from './Report'
-// export { default as Prediction } from './Prediction'
-// export { default as Accident } from './Accident'
-// export { default as AccidentPast } from './AccidentPast'
-
-// 
 export { default as SensorManage } from './SensorManage'
 export { default as RepeaterManage } from './RepeaterManage'
 export { default as ReceiverManage } from './ReceiverManage'
